f2a04c22a9a7d9ad903ee05fc6c336ced3acadc6
Vishal Yadav <vdragns98@gmail.com>
2021-10-11 22:02:00 +0530

    added new component

--- inverter/src/app/app.module.ts
+++ inverter/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InverterappComponent } from './inverterapp/inverterapp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InverterappComponent
   ],
   imports: [
     BrowserModule,
