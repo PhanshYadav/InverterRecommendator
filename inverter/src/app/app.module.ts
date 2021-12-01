8085bcf6c31cb1006ac4a9e73f1e0dab9973f7df
Vishal Yadav <vdragns98@gmail.com>
2021-12-01 12:16:23 +0530

    expenses

--- inverter/src/app/app.module.ts
+++ inverter/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InverterappComponent } from './inverterapp/inverterapp.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    InverterappComponent
+    CalculatorComponent,
   ],
   imports: [
     BrowserModule,
--- inverter/src/app/calculator/calculator.component.spec.ts
+++ inverter/src/app/calculator/calculator.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { InverterappComponent } from './inverterapp.component';
+import { CalculatorComponent } from './calculator.component';
 
-describe('InverterappComponent', () => {
-  let component: InverterappComponent;
-  let fixture: ComponentFixture<InverterappComponent>;
+describe('CalculatorComponent', () => {
+  let component: CalculatorComponent;
+  let fixture: ComponentFixture<CalculatorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ InverterappComponent ]
+      declarations: [ CalculatorComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(InverterappComponent);
+    fixture = TestBed.createComponent(CalculatorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
